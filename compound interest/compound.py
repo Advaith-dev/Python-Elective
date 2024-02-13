fdcfcd5d3fb0791c0ab8358e164a9d35a29be096
Advaith Manoj <89682610+Advaith-dev@users.noreply.github.com>
2024-02-13 15:41:07 +0530

    final version

--- compound interest/compound.py
+++ compound interest/compound.py
@@ -7,5 +7,5 @@ print("Year\tStarting balance\tInterest\tEnding Balance")
 for x in range(t):
     i = p * (r/100)
     new = p + i
-    print(x+1,"\t",p,"\t","%0.2f" % i,"\t","%0.2f" % new)
+    print(x+1,"\t",p,"\t\t","%0.2f" % i,"\t","%0.2f" % new)
     p = new
