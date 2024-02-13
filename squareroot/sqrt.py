6a9f2f1060021f8e6017b6bf1ce4a822fba2bdbc
Advaith Manoj <89682610+Advaith-dev@users.noreply.github.com>
2024-02-13 21:37:22 +0530

    Comments added

--- squareroot/sqrt.py
+++ squareroot/sqrt.py
@@ -1,7 +1,19 @@
+''''    square root algorithm by Sir Issac Newton
+    z = 1 , initially the value of z is one
+    let x be a positive floating point number 
+    let y be the actual square root of x
+    perform z = (z + x/ z) / 2, till the value for z becomes close to y
+'''
+
 num = float(input("Enter the positive number: "))
+
+#square root calculated by direct method
 py_estimate = num **(1/2)
+
+#
 z = 1
 
+#Repeating until 7 decimal points become same
 while "%0.7f" %  z != "%0.7f" %  py_estimate:
     z = (z + num / z) / 2
 
